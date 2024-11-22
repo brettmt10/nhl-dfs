ff40fd489e7a7b3287901f6187eec37707fe617a
brettmt10 <brettmt10@gmail.com>
2024-11-22 14:13:10 -0500

    json path for extracting matchups

--- scraper.py
+++ scraper.py
@@ -3,30 +3,44 @@ from bs4 import BeautifulSoup
 import teamLinks
 import pandas as pd
 from io import StringIO
+from nhlpy import NHLClient
 
-# source set up
-url = teamLinks.links["seattle"]
-response = requests.get(url)
-html = response.text
-soup = BeautifulSoup(html, "lxml")
+class Scraper():
+    def __init__(self):
+        self.client = NHLClient()
+        self.schedule = self.client.schedule.get_schedule(date="2024-11-22")
+        
+        def get_matchup(i):
+            # json path to extract matchups
+            matchup_json_path = self.schedule.get('games')[i].get('awayTeam').get('placeName')['default']
+            return matchup_json_path
+        
+    def get_team_player_data():
+        # source set up
+        url = teamLinks.links["seattle"]
+        response = requests.get(url)
+        html = response.text
+        soup = BeautifulSoup(html, "lxml")
 
-# scrape wanted elements
-html_div = soup.find('div', {"id": "div_player_stats"}) # div containing the table
-html_table_headers = html_div.find('table').find('thead').find_all('tr')[1].find_all('th') # headers of table
-html_table_body = html_div.find('table') # body of the table
-html_table_str = StringIO(str(html_table_body)) # table as a StringIO input for pandas
+        # scrape wanted elements
+        html_div = soup.find('div', {"id": "div_player_stats"}) # div containing the table
+        html_table_headers = html_div.find('table').find('thead').find_all('tr')[1].find_all('th') # headers of table
+        html_table_body = html_div.find('table') # body of the table
+        html_table_str = StringIO(str(html_table_body)) # table as a StringIO input for pandas
 
-# create clean list of headers
-clean_headers = []
-for header_value in html_table_headers:
-    clean_headers.append(header_value.text.strip())
+        # create clean list of headers
+        clean_headers = []
+        for header_value in html_table_headers:
+            clean_headers.append(header_value.text.strip())
 
-# convert to pandas, replace headers with clean headers
-player_data = pd.DataFrame(pd.read_html(html_table_str)[0])
-player_data.columns=clean_headers
+        # convert to pandas, replace headers with clean headers
+        player_data = pd.DataFrame(pd.read_html(html_table_str)[0])
+        player_data.columns=clean_headers
 
-# remove unneccesary columns
-player_data = player_data.drop(["Rk", "Awards"], axis=1)
+        # remove unneccesary columns
+        player_data = player_data.drop(["Rk", "Awards"], axis=1)
 
-# remove team totals row
-player_data = player_data.iloc[:-1]
+        # remove team totals row
+        player_data = player_data.iloc[:-1]
+        
+scraper = Scraper()
