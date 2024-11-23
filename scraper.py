8230e78b8bca9dcd98e0b1694c878f0f6e641e51
brettmt10 <brettmt10@gmail.com>
2024-11-22 19:54:50 -0500

    refactor functions in scraper class

--- scraper.py
+++ scraper.py
@@ -13,42 +13,45 @@ class Scraper():
         self.schedule = self.client.schedule.get_schedule(date="2024-11-22").get('games')
         self.num_games = len(self.schedule)
         self.matchups = []
-        
-        # get time for each match up, based on utc values
-        def get_matchup_time(match_time):
-            # parse UTC string
-            time = datetime.strptime(match_time, '%Y-%m-%dT%H:%M:%SZ')
-            time = pytz.utc.localize(time)
-            
-            # convert format time into EST
-            timezone = pytz.timezone('America/New_York')
-            formatted_match_time = time.astimezone(timezone).strftime('%I:%M %p')
-            
-            return(formatted_match_time)
-            
-        # get all matchup info (not player data)
-        def get_all_matchups(schedule, i):
-            # json paths to extract matchups
-            away_team = schedule[i].get('awayTeam').get('placeName')['default'].lower()
-            home_team = schedule[i].get('homeTeam').get('placeName')['default'].lower()
-            logo_away_team = schedule[i].get('homeTeam').get('logo')
-            logo_home_team = schedule[i].get('awayTeam').get('logo')
-            
-            matchup_utc = schedule[i].get('startTimeUTC')
-            matchup_start_time = get_matchup_time(matchup_utc)
-            
-            return {"away_team": {"city": away_team, "logo": logo_away_team},
-                    "home_team": {"city": home_team, "logo": logo_home_team},
-                    "start_time": matchup_start_time}
-        
-        for i in range(self.num_games):
-            self.matchups.append(get_all_matchups(self.schedule, i))
             
     def get_daily_matchups(self):
         return self.matchups
     
     def get_num_games(self):
         return self.num_games
+    
+    # get time for each match up, based on utc values
+    def get_matchup_time(self, match_time):
+        # parse UTC string
+        time = datetime.strptime(match_time, '%Y-%m-%dT%H:%M:%SZ')
+        time = pytz.utc.localize(time)
+        
+        # convert format time into EST
+        timezone = pytz.timezone('America/New_York')
+        formatted_match_time = time.astimezone(timezone).strftime('%I:%M %p')
+        
+        return(formatted_match_time)
+    
+    
+    # get matchup info (not player data)
+    def get_matchup(self, schedule, i):
+        # json paths to extract matchups
+        away_team = schedule[i].get('awayTeam').get('placeName')['default'].lower()
+        home_team = schedule[i].get('homeTeam').get('placeName')['default'].lower()
+        logo_away_team = schedule[i].get('homeTeam').get('logo')
+        logo_home_team = schedule[i].get('awayTeam').get('logo')
+        
+        matchup_utc = schedule[i].get('startTimeUTC')
+        matchup_start_time = self.get_matchup_time(matchup_utc)
+        
+        return {"away_team": {"city": away_team, "logo": logo_away_team},
+                "home_team": {"city": home_team, "logo": logo_home_team},
+                "start_time": matchup_start_time}
+        
+    # get each matchup's info 
+    def get_all_matchups(self):
+        for i in range(self.num_games):
+            self.matchups.append(self.get_all_matchups(self.schedule, i))
                 
     def get_team_player_data():
         # source set up
